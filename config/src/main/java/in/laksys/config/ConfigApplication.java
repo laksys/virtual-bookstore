9735a2ad2da35b9a19513f1363fa7879f883fb93
lakshmanan <laksys@gmail.com>
2026-04-29 13:15:19 +0530

    initial commit

---
+++ config/src/main/java/in/laksys/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package in.laksys.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+	private static final Logger logger =LoggerFactory.getLogger(ConfigApplication.class);
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConfigApplication.class, args);
+		logger.info("config server started...");
+	}
+
+}
---
+++ config/src/test/java/in/laksys/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package in.laksys.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
